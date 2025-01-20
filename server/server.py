fb891f6e1e584ade99a9a4f03ed8569bec34641c
kds.lo <96259174+LellerLololol@users.noreply.github.com>
2025-01-20 18:55:49 +0200

    back-end updates

--- server/server.py
+++ server/server.py
@@ -1,1 +1,30 @@
 import flask
+import json
+import os
+
+
+def main():
+    app = flask(__name__)
+
+    # File path for JSON data
+    JSON_PATH = 'data.json'
+
+    # Helper function to read the JSON file
+    def read_json_data():
+        if os.path.exists(JSON_PATH):
+            with open(JSON_PATH, 'r') as file:
+                return json.load(file)
+        else:
+            return []
+        
+    # Route to get all offers
+    @app.route('/api/getAllOffers', methods=['GET'])
+    def get_users():
+        offers = read_json_data()
+        return flask.jsonify(offers), 200
+
+    app.run(debug=True, port=80)
+
+
+if __name__ == '__main__':
+    main()
