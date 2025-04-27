69ca29296ca8655dcecc963d7453a965b89729d0
LellerLololol <mikehideo.kahar@gmail.com>
2025-04-27 12:49:46 +0300

    multiple fixes

--- server/get_image.php
+++ server/get_image.php
@@ -6,8 +6,6 @@ ini_set('display_errors', 0);
 
 $file_path = $argv[1];
 
-echo getcwd() . "\n";
-
 if (file_exists($file_path)) {
   $image_data = file_get_contents($file_path);
   echo base64_encode($image_data);
